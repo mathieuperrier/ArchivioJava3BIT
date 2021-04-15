dc3702a81b6de8ed7df943f35201640f044a04b4
matok <matok@LAPTOP-4P6HKU6D>
2021-04-15 17:12:07 +0200

    Modificato classe Scarpa

---
+++ ArchivioJava3BIT/src/archiviojava3bit/Scarpa.java
@@ -0,0 +1,18 @@
+package archiviojava3bit;
+
+/**
+ * Classe utilizzata per l'oggetto da contenere nella collezione.
+ * Contiene le varibili di istanza, il metodo costruttore, i setter/getter
+ * e eventuali altri metodi
+ * @author Perrier Mathieu
+ */
+public class Scarpa {
+    //mettere caratteristiche (4-5) es. prezzo, marca, ecc...
+    private String codice; //es. ABC100A
+    private String marca;
+    private String colore;
+    private float prezzo;
+    private boolean isScontato;
+    private int quantità;
+    
+}
