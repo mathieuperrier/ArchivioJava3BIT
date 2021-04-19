5e34bd3e5f1c6c80d4aee067d23a6980742f3742
matok <matok@LAPTOP-4P6HKU6D>
2021-04-19 13:12:05 +0200

    Modificato classe Scarpa e iniziato Archivio

--- ArchivioJava3BIT/src/archiviojava3bit/Scarpa.java
+++ ArchivioJava3BIT/src/archiviojava3bit/Scarpa.java
@@ -7,12 +7,68 @@ package archiviojava3bit;
  * @author Perrier Mathieu
  */
 public class Scarpa {
-    //mettere caratteristiche (4-5) es. prezzo, marca, ecc...
     private String codice; //es. ABC100A
     private String marca;
     private String colore;
     private float prezzo;
     private boolean isScontato;
-    private int quantità;
+    private int quantita;
+    
+    public Scarpa(String codice, String marca, String colore, float prezzo, boolean isScontato, int quantita) {
+        this.codice=codice;
+        this.marca=marca;
+        this.colore=colore;
+        this.prezzo=prezzo;
+        this.isScontato=isScontato;
+        this.quantita=quantita;
+    }
+    
+    public String getCodice() {
+        return codice;
+    }
+    
+    public void setCodice() {
+        this.codice=codice;
+    }
+    
+    public String getMarca() {
+        return marca;
+    }
+    
+    public void setMarca() {
+        this.marca=marca;
+    }
+    
+    public String getColore() {
+        return colore;
+    }
+    
+    public void setColore() {
+        this.colore=colore;
+    }
+    
+    public float getPrezzo() {
+        return prezzo;
+    }
+    
+    public void setPrezzo() {
+        this.prezzo=prezzo;
+    }
+    
+    public boolean getIsScontato() {
+        return isScontato;
+    }
+    
+    public void setIsScontato() {
+        this.isScontato=isScontato;
+    }
+    
+    public int getQuantita() {
+        return quantita;
+    }
+    
+    public void setQuantita() {
+        this.quantita=quantita;
+    }
     
 }
