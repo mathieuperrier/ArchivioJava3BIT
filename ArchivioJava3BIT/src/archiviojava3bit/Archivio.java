85e2bdcced3ef70791c73f6a7c6d63ba74d93641
matok <matok@LAPTOP-4P6HKU6D>
2021-04-26 13:13:05 +0200

    Modificato Archivio

--- ArchivioJava3BIT/src/archiviojava3bit/Archivio.java
+++ ArchivioJava3BIT/src/archiviojava3bit/Archivio.java
@@ -1,13 +1,32 @@
 package archiviojava3bit;
 
+import java.util.ArrayList;
+
 /**
  * Contiene gli oggetti del programma.
  * Implementata tramite un ArrayList.
  * Ci permette di inserire gli oggetti,
  * effettuare ricerche e
  * aggiungere/rimuovere/modificare un oggetto.
- * @author INSERISCI_NOME
+ * @author Perrier Mathieu
  */
 public class Archivio {
+    private ArrayList<Scarpa> archivio;
     
-}
+    public Archivio() {
+        archivio = new ArrayList(0);
+    }
+    
+    public void aggiungi(Scarpa scarpa) {
+        archivio.add(scarpa);
+    }
+    
+    public Scarpa cerca(String codice) {
+        for(int i=0; i<archivio.size(); i++) {
+            Scarpa scarpa = archivio.get(i);
+            if(scarpa.getCodice()==codice) {
+                return scarpa;
+            }
+        }
+    }
+   }
